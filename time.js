982047016999ced2dddb42597184c5a3a6db02d1
Furkan Bitik <furkanbitik2@gmail.com>
2023-11-01 20:26:12 +0300

    u

--- time.js
+++ time.js
@@ -1,4 +1,4 @@
-// Dom Elements
+// Selecting DOM elements
 const daysRemaining = document.getElementById("days");
 const hoursRemaining = document.getElementById("hours");
 const minutesRemaining = document.getElementById("minutes");
@@ -6,46 +6,42 @@ const secondsRemaining = document.getElementById("seconds");
 
 const msg = document.querySelector(".message");
 
-
 // Display Date Function
 function displayDate() {
-  // Current Date
-  let currentDate = new Date();
-  let destinationDate = new Date(2022, 5, 3); // 3 Haziran 2022
+ // Current Date
+ let currentDate = new Date();
+ let destinationDate = new Date(2022, 5, 3); // 3 Haziran 2022
 
-  // Difference in Milliseconds
-  let difference = currentDate - destinationDate;
+ // Difference in Milliseconds
+ let difference = currentDate - destinationDate;
 
-  // Convert milliseconds to seconds
-  let seconds = Math.floor(difference / 1000);
+ // Convert milliseconds to seconds
+ let seconds = Math.floor(difference / 1000);
 
-  // Calculate days, hours, minutes, and remaining seconds
-  let days = Math.floor(seconds / (24 * 60 * 60));
-  let hours = Math.floor((seconds % (24 * 60 * 60)) / (60 * 60));
-  let minutes = Math.floor((seconds % (60 * 60)) / 60);
-  let remainingSeconds = seconds % 60;
+ // Calculate days, hours, minutes, and remaining seconds
+ let days = Math.floor(seconds / (24 * 60 * 60));
+ let hours = Math.floor((seconds % (24 * 60 * 60)) / (60 * 60));
+ let minutes = Math.floor((seconds % (60 * 60)) / 60);
+ let remainingSeconds = Math.floor(seconds % 60);
 
-  // Update DOM elements
-  daysRemaining.textContent = days;
-  hoursRemaining.textContent = hours;
-  minutesRemaining.textContent = minutes;
-  secondsRemaining.textContent = remainingSeconds;
+ // Update the display
+ daysRemaining.textContent = days;
+ hoursRemaining.textContent = hours;
+ minutesRemaining.textContent = minutes;
+ secondsRemaining.textContent = remainingSeconds;
+
+ // Check if the event has already occurred
+ if (difference > 0) {
+ clearInterval(countdown);
+ msg.textContent = "Congratulations! The wedding is over!";
+ }
 }
 
-
-// Initial Call
+// Start the countdown
 displayDate();
-// Call Every 1 Second
 setInterval(displayDate, 1000);
 
-// Dom Element Fot Light-Dark Mode Toogler
-document.querySelector("#light-dark-toogle").onclick = function (e) {
-  // Giving The Class dark to the Body
-  document.body.classList.toggle("dark");
-  // Toggle The Moon Shape
-  e.target.classList.toggle("fa-moon");
-  // Toggle The Sun Shape
-  e.target.classList.toggle("fa-sun");
-
-  
-};
+// Toggle Light-Dark Theme
+document.getElementById("light-dark-toogle").addEventListener("click", function () {
+ document.body.classList.toggle("dark");
+});
