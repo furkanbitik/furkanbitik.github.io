e8f3668b7e5021bd6f0df24ea1501a4112f918c8
Furkan Bitik <furkanbitik2@gmail.com>
2023-11-01 21:59:58 +0300

    u

--- time.js
+++ time.js
@@ -1,43 +1,43 @@
-(function () {
-  const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
+const countdown = new Date("June 3, 2022");
 
-  //I'm adding this section so I don't have to keep updating this pen every year :-)
-  //remove this if you don't need it
-  let today = new Date(),
-      dd = String(today.getDate()).padStart(2, "0"),
-      mm = String(today.getMonth() + 1).padStart(2, "0"),
-      yyyy = today.getFullYear(),
-      nextYear = yyyy + 1,
-      dayMonth = "09/30/",
-      birthday = dayMonth + yyyy;
+function getRemainingTime(endtime) {
+  const milliseconds = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor( (milliseconds/1000) % 60 );
+  const minutes = Math.floor( (milliseconds/1000/60) % 60 );
+  const hours = Math.floor( (milliseconds/(1000*60*60)) % 24 );
+  const days = Math.floor( milliseconds/(1000*60*60*24) );
+
+  return {
+    'total': milliseconds,
+    'seconds': seconds,
+    'minutes': minutes,
+    'hours': hours,
+    'days': days,
+  };
+}
   
-  today = mm + "/" + dd + "/" + yyyy;
-  if (today > birthday) {
-    birthday = dayMonth + nextYear;
+function initClock(id, endtime) {
+  const counter = document.getElementById(id);
+  const daysItem = counter.querySelector('.js-countdown-days');
+  const hoursItem = counter.querySelector('.js-countdown-hours');
+  const minutesItem = counter.querySelector('.js-countdown-minutes');
+  const secondsItem = counter.querySelector('.js-countdown-seconds');
+
+  function updateClock() {
+    const time = getRemainingTime(endtime);
+
+    daysItem.innerHTML = time.days;
+    hoursItem.innerHTML = ('0' + time.hours).slice(-2);
+    minutesItem.innerHTML = ('0' + time.minutes).slice(-2);
+    secondsItem.innerHTML = ('0' + time.seconds).slice(-2);
+
+    if (time.total <= 0) {
+      clearInterval(timeinterval);
+    }
   }
-  //end
-  
-  const countDown = new Date(birthday).getTime(),
-      x = setInterval(function() {    
 
-        const now = new Date().getTime(),
-              distance = countDown - now;
+  updateClock();
+  const timeinterval = setInterval(updateClock, 1000);
+}
 
-        document.getElementById("days").innerText = Math.floor(distance / (day)),
-          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-        //do something later when date is reached
-        if (distance < 0) {
-          document.getElementById("headline").innerText = "It's my birthday!";
-          document.getElementById("countdown").style.display = "none";
-          document.getElementById("content").style.display = "block";
-          clearInterval(x);
-        }
-        //seconds
-      }, 0)
-  }());
+initClock('js-countdown', countdown);
