13160c1810954b8184b4856453b2575ba6ddeeaa
Furkan Bitik <furkanbitik2@gmail.com>
2023-11-02 04:50:01 +0300

    update new

--- time.js
+++ time.js
@@ -1,119 +1,20 @@
-var ringer = {
-  countdown_to: "06/03/2022",
-  rings: {
-    DAYS: {
-      s: 86400000, // mseconds in a day,
-      max: 365
-    },
-    HOURS: {
-      s: 3600000, // mseconds per hour,
-      max: 24
-    },
-    MINUTES: {
-      s: 60000, // mseconds per minute
-      max: 60
-    },
-    SECONDS: {
-      s: 1000,
-      max: 60
-    },
-    MICROSEC: {
-      s: 10,
-      max: 100
-    }
-  },
-  r_count: 5,
-  r_spacing: 10, // px
-  r_size: 100, // px
-  r_thickness: 5, // px
-  update_interval: 11, // ms
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
 
-  init: function () {
-    $r = ringer;
-    $r.cvs = document.createElement("canvas");
+function countdown() {
 
-    $r.size = {
-      w:
-        ($r.r_size + $r.r_thickness) * $r.r_count +
-        $r.r_spacing * ($r.r_count - 1),
-      h: $r.r_size + $r.r_thickness
-    };
+    let differenceDate = Date.parse('2023-04-25T19:35:13.200Z') - Date.now();
 
-    $r.cvs.setAttribute("width", $r.size.w);
-    $r.cvs.setAttribute("height", $r.size.h);
-    $r.ctx = $r.cvs.getContext("2d");
-    $(".countdownwrap").append($r.cvs);
-    $r.cvs = $($r.cvs);
-    $r.ctx.textAlign = "center";
-    $r.actual_size = $r.r_size + $r.r_thickness;
-    $r.countdown_to_time = new Date($r.countdown_to).getTime();
-    $r.cvs.css({ width: $r.size.w + "px", height: $r.size.h + "px" });
-    $r.go();
-  },
-  ctx: null,
-  go: function () {
-    var idx = 0;
+    const days = Math.floor(differenceDate / DAY);
+    differenceDate -= days * DAY;
+    const hours = Math.floor(differenceDate / HOUR);
+    differenceDate -= hours * HOUR;
+    const minutes = Math.floor(differenceDate / MINUTE);
 
-    $r.time = new Date().getTime() - $r.countdown_to_time;
+    document.querySelector('.js-days').innerHTML = days;
+    document.querySelector('.js-hours').innerHTML = hours;
+    document.querySelector('.js-minutes').innerHTML = minutes;
+}
 
-    for (var r_key in $r.rings) $r.unit(idx++, r_key, $r.rings[r_key]);
-
-    setTimeout($r.go, $r.update_interval);
-  },
-  unit: function (idx, label, ring) {
-    var x,
-      y,
-      value,
-      ring_secs = ring.s;
-    value = parseFloat($r.time / ring_secs);
-    $r.time -= Math.round(parseInt(value)) * ring_secs;
-    value = Math.abs(value);
-
-    x = $r.r_size * 0.5 + $r.r_thickness * 0.5;
-    x += +(idx * ($r.r_size + $r.r_spacing + $r.r_thickness));
-    y = $r.r_size * 0.5;
-    y += $r.r_thickness * 0.5;
-
-    // calculate arc end angle
-    var degrees = 360 - (value / ring.max) * 360.0;
-    var endAngle = degrees * (Math.PI / 180);
-
-    $r.ctx.save();
-
-    $r.ctx.translate(x, y);
-    $r.ctx.clearRect(
-      $r.actual_size * -0.5,
-      $r.actual_size * -0.5,
-      $r.actual_size,
-      $r.actual_size
-    );
-
-    // first circle
-    $r.ctx.strokeStyle = "rgb(255, 33, 33)";
-    $r.ctx.beginPath();
-    $r.ctx.arc(0, 0, $r.r_size / 2, 0, 2 * Math.PI, 2);
-    $r.ctx.lineWidth = $r.r_thickness;
-    $r.ctx.stroke();
-
-    // second circle
-    $r.ctx.strokeStyle = "rgb(100, 0, 0)";
-    $r.ctx.beginPath();
-    $r.ctx.arc(0, 0, $r.r_size / 2, 0, endAngle, 1);
-    $r.ctx.lineWidth = $r.r_thickness;
-    $r.ctx.stroke();
-
-    // label
-    $r.ctx.fillStyle = "#000000";
-
-    $r.ctx.font = "12px Helvetica";
-    $r.ctx.fillText(label, 0, 23);
-    $r.ctx.fillText(label, 0, 23);
-
-    $r.ctx.font = "bold 40px Helvetica";
-    $r.ctx.fillText(Math.floor(value), 0, 10);
-
-    $r.ctx.restore();
-  }
-};
-
-ringer.init();
+setInterval(countdown, 1000);
