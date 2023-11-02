b4a4e31f099da077a87ddf4384d5297da5950bee
Furkan Bitik <furkanbitik2@gmail.com>
2023-11-02 04:52:02 +0300

    Update time.js

--- time.js
+++ time.js
@@ -1,9 +1,10 @@
+const SECOND = 1000;
 const MINUTE = 60 * SECOND;
 const HOUR = 60 * MINUTE;
 const DAY = 24 * HOUR;
 
 function countdown() {
-
+    // 25/04/23 02:00
     let differenceDate = Date.parse('2023-04-25T19:35:13.200Z') - Date.now();
 
     const days = Math.floor(differenceDate / DAY);
@@ -11,10 +12,13 @@ function countdown() {
     const hours = Math.floor(differenceDate / HOUR);
     differenceDate -= hours * HOUR;
     const minutes = Math.floor(differenceDate / MINUTE);
+    differenceDate -= minutes * MINUTE;
+    const seconds = Math.floor(differenceDate / SECOND);
 
     document.querySelector('.js-days').innerHTML = days;
     document.querySelector('.js-hours').innerHTML = hours;
     document.querySelector('.js-minutes').innerHTML = minutes;
+    document.querySelector('.js-seconds').innerHTML = seconds;
 }
 
 setInterval(countdown, 1000);
